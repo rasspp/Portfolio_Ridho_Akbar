1d0595f587d92985d3f4b97aec9bc0a73d1b4a2c
ridhoakbar8977 <ridho8977@gmail.com>
2023-07-29 19:09:18 +0700

    upadate create root v 18

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom";
+/* eslint-disable no-undef */
+// import React from "react";
+// import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { StrictMode } from "react";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-   <React.StrictMode>
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//    <React.StrictMode>
+//       <App />
+//    </React.StrictMode>
+// );
+
+const root = createRoot(document.getElementById("root"));
+root.render(
+   <StrictMode>
       <App />
-   </React.StrictMode>
+   </StrictMode>
 );
 
 reportWebVitals();
